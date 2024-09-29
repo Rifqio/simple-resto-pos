be832e4e9e8e7c1719e809e88a7df101909d25a5
rifqio <rifqi.oktario@dansmultipro.com>
2024-09-29 22:41:03 +0700

    feat(dashboard): finishing the sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core'
 import { NgIconComponent, provideIcons } from '@ng-icons/core'
-import { heroChevronDoubleRightSolid, heroHomeSolid } from '@ng-icons/heroicons/solid'
+import {
+    heroChevronDoubleRightSolid,
+    heroCogSolid,
+    heroDocumentSolid,
+    heroHomeSolid,
+    heroSquares2x2Solid,
+    heroUserCircleSolid
+} from '@ng-icons/heroicons/solid'
 
 @Component({
     selector: 'component-sidebar',
     standalone: true,
     imports: [NgIconComponent],
-    providers: [provideIcons({ heroHomeSolid, heroChevronDoubleRightSolid })],
+    providers: [
+        provideIcons({
+            heroHomeSolid,
+            heroChevronDoubleRightSolid,
+            heroSquares2x2Solid,
+            heroUserCircleSolid,
+            heroDocumentSolid,
+            heroCogSolid
+        })
+    ],
     templateUrl: './sidebar.component.html'
 })
-
 export class SidebarComponent {
-    protected items = [{ icon: 'heroHomeSolid', label: 'Home', link: '/' }]
+    protected items = [
+        { icon: 'heroHomeSolid', label: 'Home', link: '/' },
+        {
+            icon: 'heroSquares2x2Solid',
+            label: 'Categories',
+            link: '/categories'
+        },
+        { icon: 'heroUserCircleSolid', label: 'Orders', link: '/orders' },
+        { icon: 'heroDocumentSolid', label: 'Profile', link: '/profile' },
+        { icon: 'heroCogSolid', label: 'Settings', link: '/settings' }
+    ]
 }
